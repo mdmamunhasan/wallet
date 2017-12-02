8e69546025f4de7065af8f3914316c5bcac64371
mamun <mamunthecoder@gmail.com>
2017-12-02 23:37:14 +0600

    dialog

--- app/src/main/java/com/etecharena/wallet/activities/AccountActivity.java
+++ app/src/main/java/com/etecharena/wallet/activities/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.etecharena.wallet.activities;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.etecharena.wallet.R;
+import com.etecharena.wallet.fragments.ItemDialogFragment;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -22,10 +24,14 @@ public class AccountActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showItemDialog();
             }
         });
+    }
+
+    public void showItemDialog() {
+        ItemDialogFragment itemDialog = new ItemDialogFragment();
+        itemDialog.show(getSupportFragmentManager(), "update");
     }
 
 }
