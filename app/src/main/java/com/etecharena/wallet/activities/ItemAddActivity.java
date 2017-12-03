f8f6c285a9372f8faa04452b5188f45880b4ffd2
mamun <mamunthecoder@gmail.com>
2017-12-03 18:54:32 +0600

    save button

--- app/src/main/java/com/etecharena/wallet/activities/ItemAddActivity.java
+++ app/src/main/java/com/etecharena/wallet/activities/ItemAddActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.etecharena.wallet.R;
 import com.etecharena.wallet.fragments.DatePickerFragment;
@@ -15,11 +16,23 @@ public class ItemAddActivity extends AppCompatActivity implements DatePickerFrag
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_add);
+
+        Button mSaveButton = (Button) findViewById(R.id.save_button);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveItem();
+            }
+        });
     }
 
     public void showDatePickerDialog(View v) {
         DatePickerFragment newFragment = DatePickerFragment.newInstance(this);
         newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    public void saveItem() {
+        Log.d("Save", "save");
     }
 
     @Override
