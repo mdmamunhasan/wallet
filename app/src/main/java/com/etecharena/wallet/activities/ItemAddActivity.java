b580c003c0160959502031d5c0fad4d1b1c74038
mamun <mamunthecoder@gmail.com>
2017-12-03 19:16:02 +0600

    save button

--- app/src/main/java/com/etecharena/wallet/activities/ItemAddActivity.java
+++ app/src/main/java/com/etecharena/wallet/activities/ItemAddActivity.java
@@ -1,5 +1,9 @@
 package com.etecharena.wallet.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -59,6 +63,39 @@ public class ItemAddActivity extends AppCompatActivity implements DatePickerFrag
 
     public void saveItem() {
         Log.d("Save", "save");
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mItemFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mItemFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mItemFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mItemFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 
     @Override
