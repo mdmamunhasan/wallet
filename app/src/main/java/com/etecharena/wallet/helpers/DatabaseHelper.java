7971c07b388a1c7da485366b309ccc50d777e41d
mamun <mamunthecoder@gmail.com>
2017-12-09 13:01:24 +0600

    save button

---
+++ app/src/main/java/com/etecharena/wallet/helpers/DatabaseHelper.java
@@ -0,0 +1,25 @@
+package com.etecharena.wallet.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by mamun on 12/9/17.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
