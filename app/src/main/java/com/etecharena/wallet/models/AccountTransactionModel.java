89eac156bd18e72b4d1881b2028d134eba954daf
mamun <mamunthecoder@gmail.com>
2017-12-09 14:22:46 +0600

    save button

--- app/src/main/java/com/etecharena/wallet/models/AccountTransactionModel.java
+++ app/src/main/java/com/etecharena/wallet/models/AccountTransactionModel.java
@@ -58,4 +58,30 @@ public class AccountTransactionModel {
                 sortOrder                                 // The sort order
         );
     }
+
+    public void updateData(String title) {
+        // New value for one column
+        ContentValues values = new ContentValues();
+        values.put(WalletContract.AccountTransaction.COLUMN_NAME_TITLE, title);
+
+        // Which row to update, based on the title
+        String selection = WalletContract.AccountTransaction.COLUMN_NAME_TITLE + " LIKE ?";
+        String[] selectionArgs = {"MyTitle"};
+
+        int count = db.update(
+                WalletContract.AccountTransaction.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+
+    }
+
+    public void deleteData() {
+        // Define 'where' part of query.
+        String selection = WalletContract.AccountTransaction.COLUMN_NAME_TITLE + " LIKE ?";
+        // Specify arguments in placeholder order.
+        String[] selectionArgs = {"MyTitle"};
+        // Issue SQL statement.
+        db.delete(WalletContract.AccountTransaction.TABLE_NAME, selection, selectionArgs);
+    }
 }
