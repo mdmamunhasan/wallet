72e718e3e6bf27727bab79f5e1f658599a480120
mamun <mamunthecoder@gmail.com>
2017-12-12 23:42:46 +0600

    show data

--- app/src/main/java/com/etecharena/wallet/adapters/TransactionAdapter.java
+++ app/src/main/java/com/etecharena/wallet/adapters/TransactionAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.etecharena.wallet.R;
@@ -39,9 +40,15 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
         holder.mTitleView.setText(mEntity.getTitle());
         holder.mAmountView.setText("Tk" + mEntity.getAmount());
 
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        String strDate = formatter.format(mEntity.getTimestamp());
-        holder.mDateView.setText(strDate);
+        if (mEntity.getTimestamp() != null && mEntity.getTimestamp() > 0) {
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            String strDate = formatter.format(mEntity.getTimestamp());
+            holder.mDateView.setText(strDate);
+        }
+
+        if (mEntity.getType() != null && mEntity.getType() > 0) {
+            holder.mCardView.setBackgroundColor(0xFF00FF00);
+        }
     }
 
     @Override
@@ -50,12 +57,14 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout mCardView;
         public TextView mTitleView;
         public TextView mAmountView;
         public TextView mDateView;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            mCardView = (LinearLayout) itemView.findViewById(R.id.transaction_card);
             mTitleView = (TextView) itemView.findViewById(R.id.transaction_title);
             mAmountView = (TextView) itemView.findViewById(R.id.transaction_amount);
             mDateView = (TextView) itemView.findViewById(R.id.transaction_timestamp);
